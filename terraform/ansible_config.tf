24f2d3385b89cd75b59ba419a157db3491a5966e
Josie Lander <landerjosie@gmail.com>
2023-03-05 21:25:28 +0000

    Fix endpoint for joining to cluster

--- terraform/ansible_config.tf
+++ terraform/ansible_config.tf
@@ -3,7 +3,7 @@ resource "local_file" "ansible_vars" {
     k3s_version       = "v1.25.7-rc1+k3s1"
     ansible_user      = "ec2-user"
     systemd_dir       = "/etc/systemd/system"
-    kube_api_lb       = module.k8s-lb.lb_dns_name
+    master_endpoint   = module.ec2_instance[0].private_ip
     extra_server_args = "--tls-san ${module.k8s-lb.lb_dns_name}"
     extra_agent_args  = ""
   })

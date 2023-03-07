ff0ee15fb3d9ec800d1185b204a2eeac521541a0
Josie Lander <landerjosie@gmail.com>
2023-03-07 10:49:11 +0000

    Add worker nodes and load balancer for apps

---
+++ terraform/apps_load_balancer.tf
@@ -0,0 +1,26 @@
+module "apps-lb" {
+  source        = "./modules/terraform-aws-nlb"
+  lb_subnet_ids = module.vpc.public_subnets
+  vpc_id        = module.vpc.vpc_id
+  lb_name = "apps-lb"
+  https_enabled = true
+  http_port          = var.http_port
+  http_target_group_name = "apps-http-tg"
+  https_port          = var.https_port
+  https_target_group_name = "apps-https-tg"
+
+}
+
+resource "aws_lb_target_group_attachment" "http_worker_targets" {
+  count            = var.num_of_workers
+  target_group_arn = module.apps-lb.http_target_group_arn
+  target_id        = module.workers[count.index].id
+  port             = var.http_port
+}
+
+resource "aws_lb_target_group_attachment" "https_worker_targets" {
+  count            = var.num_of_workers
+  target_group_arn = module.apps-lb.https_target_group_arn
+  target_id        = module.workers[count.index].id
+  port             = var.https_port
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,6 +10,11 @@ output "addtitional-controllers" {
   value = try(slice(module.controllers, 1, var.num_of_controllers).*.public_ip, null)
 }
 
+output "workers" {
+  value = try(slice(module.workers, 0, var.num_of_workers).*.public_ip, null)
+}
+
+
 output "kube_api_lb" {
   value = module.k8s-lb.lb_dns_name
 }
